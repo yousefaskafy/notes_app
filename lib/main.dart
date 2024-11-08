ada9aca938a94b224729c22b3f7c91c91a9f442d
yousefaskafy <154416844+yousefaskafy@users.noreply.github.com>
2024-11-08 14:31:03 +0200

    create Notes List View

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/notes_view.dart';
 
 void main() {
   runApp(const NotesApp());
@@ -10,8 +11,8 @@ class NotesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(brightness: Brightness.dark),
-      home: const NotesApp(),
+      theme: ThemeData(brightness: Brightness.dark,fontFamily: 'poppins'),
+      home: const NotesView(),
     );
   }
 }
---
+++ lib/widgets/custom_app_bar.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/custom_icon.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      children: [
+        Text(
+          'Notes',
+          style: TextStyle(fontSize: 20),
+        ),
+        Spacer(),
+        CustomSearchIcon(),
+      ],
+    );
+  }
+}
