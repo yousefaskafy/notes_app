19f81fa08718f68c73a31b48482915f96d2b6f76
yousefaskafy <154416844+yousefaskafy@users.noreply.github.com>
2024-11-09 11:39:37 +0200

    create edit note view

--- lib/widgets/custom_note_item.dart
+++ lib/widgets/custom_note_item.dart
@@ -1,52 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
-      decoration: BoxDecoration(
-        color: const Color(0xffFFCC80),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text('flutterr tips',
-                style: TextStyle(
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return const EditNoteView();
+        }));
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: const Color(0xffFFCC80),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text('flutterr tips',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 26,
+                  )),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16, bottom: 16),
+                child: Text(
+                  'Build your career witg fltter',
+                  style: TextStyle(
+                    color: Colors.black.withOpacity(0.4),
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  FontAwesomeIcons.trash,
                   color: Colors.black,
-                  fontSize: 26,
-                )),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16, bottom: 16),
-              child: Text(
-                'Build your career witg fltter',
-                style: TextStyle(
-                  color: Colors.black.withOpacity(0.4),
-                  fontSize: 18,
+                  size: 24,
                 ),
               ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                FontAwesomeIcons.trash,
-                color: Colors.black,
-                size: 24,
+            const Padding(
+              padding: EdgeInsets.only(right: 24),
+              child: Text(
+                'may21 , 2024',
+                style: TextStyle(color: Colors.black, fontSize: 16),
               ),
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.only(right: 24),
-            child: Text(
-              'may21 , 2024',
-              style: TextStyle(color: Colors.black, fontSize: 16),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
