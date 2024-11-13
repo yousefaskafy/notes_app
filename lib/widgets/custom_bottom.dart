a4daeff928eee740a151dd5f74709b3024da4301
yousefaskafy <154416844+yousefaskafy@users.noreply.github.com>
2024-11-13 10:45:41 +0200

    change add not loading

--- lib/widgets/custom_bottom.dart
+++ lib/widgets/custom_bottom.dart
@@ -1,10 +1,10 @@
-
 import 'package:flutter/material.dart';
 import 'package:notes_app/constants.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key, this.onTap});
- final void Function()? onTap;
+  const CustomButton({super.key, this.onTap, this.isLoading = false});
+  final void Function()? onTap;
+  final bool isLoading;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -14,11 +14,21 @@ class CustomButton extends StatelessWidget {
         height: 55,
         decoration: BoxDecoration(
             color: kPrimaryColor, borderRadius: BorderRadius.circular(16)),
-        child: const Center(
-            child: Text(
-          'Add',
-          style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
-        )),
+        child: Center(
+            child: isLoading
+                ? SizedBox(
+                  height: 24,
+                  width: 24,
+                    child: CircularProgressIndicator(
+                    color: Colors.black,
+                  ))
+                : Text(
+                    'Add',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  )),
       ),
     );
   }
